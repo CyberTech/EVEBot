ea39c17e3c946a4f4d5780580c128ae9f5f380eb
CyberTech <cybertech@gmail.com>
2008-10-30 02:29:41 +0000

    Regenerated item db with all items instead of only items avail on market. Add sql file for generating items so i don't have to search my chat logs from myself to find it again next time.

---
+++ Trunk/EVEbot/Data/EVEDB_Items.sql
@@ -0,0 +1,14 @@
+SELECT 
+concat('\t\t<Set Name="', typeName, '">', '\n',
+		'\t\t\t<Setting Name="TypeID">', typeID, '</Setting>', '\n',
+		'\t\t\t<Setting Name="GroupID">', groupID, '</Setting>', '\n',
+		'\t\t\t<Setting Name="Volume">', volume, '</Setting>', '\n',
+		'\t\t\t<Setting Name="Capacity">', capacity, '</Setting>', '\n',
+		'\t\t\t<Setting Name="PortionSize">', portionSize, '</Setting>', '\n',
+		'\t\t\t<Setting Name="BasePrice">', basePrice, '</Setting>', '\n',
+		'\t\t\t<Setting Name="MarketGroupID">', marketGroupID, '</Setting>', '\n',
+		'\t\t</Set>\n'
+		) as SetString
+FROM `invTypes`
+order by typeName
+into outfile '/tmp/list.txt'
