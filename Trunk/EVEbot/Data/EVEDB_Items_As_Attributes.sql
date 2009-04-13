656c3285b4b673c43e4515e61a2eb6385c7194d2
CyberTech <cybertech@gmail.com>
2009-04-13 23:45:34 +0000

    Updated sql script to generate itemdb

--- Trunk/EVEbot/Data/EVEDB_Items_As_Attributes.sql
+++ Trunk/EVEbot/Data/EVEDB_Items_As_Attributes.sql
@@ -1,5 +1,7 @@
+; Reference: http://wiki.eve-id.net/Database_Tables_-_Inventory
+
 SELECT 
-concat_ws('','  <Setting Name="', typeID, '"',' ItemName="', typeName, '"',' GroupID="', groupID, '"',' Volume="', volume, '"',' Capacity="', capacity, '"',' PortionSize="', portionSize, '"',' BasePrice="', basePrice, '"','>0</Setting>') as SetString
-FROM `invTypes`
-order by typeID
-into outfile '/tmp/list.txt'
+ concat_ws('','  <Setting Name="', invTypes.typeID, '"',' ItemName="', typeName, '"',' GroupID="', groupID, '"',' Volume="', volume, '"',' Capacity="', capacity, '"',' PortionSize="', portionSize, '"',' BasePrice="', basePrice, '"',' weaponRangeMultiplier="', (select valueFloat from dgmTypeAttributes where dgmTypeAttributes.typeID=invTypes.typeID and dgmTypeAttributes.attributeID = 120), '"','>0</Setting>') as SetString 
+ FROM `invTypes`
+ order by invTypes.typeID
+ into outfile '/tmp/list.txt'
