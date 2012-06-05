b67cf5cb27d58ca1482229beeefe42913b06ef0d
CyberTech <cybertech@gmail.com>
2012-06-05 00:23:21 +0000

    Improve SQL export script to minimize post-export processing.

--- Trunk/EVEbot/Data/EVEDB_Items.sql
+++ Trunk/EVEbot/Data/EVEDB_Items.sql
@@ -1,18 +1,18 @@
 SELECT
-concat('\t\t<Set Name="', typeName, '">', '\n',
+concat('\t\t<Set Name="', REPLACE(REPLACE(typeName,'&','&amp;'), '"', '&quot;'), '">', '\n',
 		'\t\t\t<Setting Name="TypeID">', typeID, '</Setting>', '\n',
 		'\t\t\t<Setting Name="GroupID">', groupID, '</Setting>', '\n',
 		'\t\t\t<Setting Name="Volume">', volume, '</Setting>', '\n',
 		'\t\t\t<Setting Name="Capacity">', capacity, '</Setting>', '\n',
 		'\t\t\t<Setting Name="PortionSize">', portionSize, '</Setting>', '\n',
 		'\t\t\t<Setting Name="BasePrice">', basePrice, '</Setting>', '\n',
-		'\t\t</Set>\n'
+		'\t\t</Set>'
 		) as SetString
-FROM `invTypes`
-order by typeName
-into outfile '/tmp/EVEDB_Items_Stable.xml'
-
+ FROM `invTypes`
+	order by typeName
+ INTO OUTFILE '/tmp/EVEDB_Items_Stable.xml'
+ FIELDS ESCAPED BY ''
+ LINES TERMINATED BY '\r\n'
 		'\t\t\t<Setting Name="MarketGroupID">', marketGroupID, '</Setting>', '\n',
 
 This export is missing all items which are not sold on the market, because concat gets rid of null rows and MarketGroupID is null
-Don't forget to replace & with &amp; in new file before committing!
--- Trunk/EVEbot/Data/EVEDB_Items_As_Attributes.sql
+++ Trunk/EVEbot/Data/EVEDB_Items_As_Attributes.sql
@@ -1,15 +1,18 @@
 ; Reference: http://wiki.eve-id.net/Database_Tables_-_Inventory
 
 SELECT
- concat_ws('','  <Setting Name="', invTypes.typeID, '"',' ItemName="', typeName, '"',' GroupID="', groupID, '"',' Metalevel="', IFNULL(IFNULL(TA.valueInt,TA.valueFloat),1), '"',' Volume="', volume, '"',' Capacity="', capacity, '"',' PortionSize="', portionSize, '"',' BasePrice="', basePrice, '"',' weaponRangeMultiplier="', (select valueFloat from dgmTypeAttributes where dgmTypeAttributes.typeID=invTypes.typeID and dgmTypeAttributes.attributeID = 120), '"','>0</Setting>') as SetString
+ 	concat_ws('','\t\t<Setting Name="', invTypes.typeID, '"',' ItemName="', REPLACE(REPLACE(typeName,'&','&amp;'), '"', '&quot;'), '"',' GroupID="', groupID, '"',' Metalevel="', IFNULL(IFNULL(TA.valueInt,TA.valueFloat),1), '"',' Volume="', volume, '"',' Capacity="', capacity, '"',' PortionSize="', portionSize, '"',' BasePrice="', basePrice, '"',' weaponRangeMultiplier="', (select valueFloat from dgmTypeAttributes where dgmTypeAttributes.typeID=invTypes.typeID and dgmTypeAttributes.attributeID = 120), '"','>0</Setting>') as SetString
  FROM `invTypes`
- left outer join dgmTypeAttributes as TA on TA.typeID = invTypes.typeID and TA.attributeID=633
- order by invTypes.typeID
- into outfile '/tmp/EVEDB_Items.xml'
+ 	left outer join dgmTypeAttributes as TA on TA.typeID = invTypes.typeID and TA.attributeID=633
+ 	order by invTypes.typeID
+ INTO OUTFILE '/tmp/EVEDB_Items.xml'
+ FIELDS ESCAPED BY ''
+ LINES TERMINATED BY '\r\n'
 
 ; attributeID 633 is metalevel
 
 ; Dont forget to replace & with &amp; in new file before committing!
+; " => &quot;
 
 ;SELECT stationID, solarSystemID, stationName
 ;FROM staStations
