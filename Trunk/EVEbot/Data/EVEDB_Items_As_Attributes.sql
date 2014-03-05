0717cbb1d742a014baded4f869529fbd9bf86c8b
CyberTech <cybertech@gmail.com>
2014-03-05 08:55:56 +0000

    Update EVEDB_Items.xml for stable and trunk for Rubicon 1.1.2; Improve SQL query; Add TechLevel to output; weaponRangeMultiplier now has a default value of 1 (as did techlevel and metalevel)

--- Trunk/EVEbot/Data/EVEDB_Items_As_Attributes.sql
+++ Trunk/EVEbot/Data/EVEDB_Items_As_Attributes.sql
@@ -1,13 +1,26 @@
 ; Reference: http://wiki.eve-id.net/Database_Tables_-_Inventory
 
 SELECT
- 	concat_ws('','\t\t<Setting Name="', invTypes.typeID, '"',' ItemName="', REPLACE(REPLACE(typeName,'&','&amp;'), '"', '&quot;'), '"',' GroupID="', groupID, '"',' Metalevel="', IFNULL(IFNULL(TA.valueInt,TA.valueFloat),1), '"',' Volume="', volume, '"',' Capacity="', capacity, '"',' PortionSize="', portionSize, '"',' BasePrice="', basePrice, '"',' weaponRangeMultiplier="', (select valueFloat from dgmTypeAttributes where dgmTypeAttributes.typeID=invTypes.typeID and dgmTypeAttributes.attributeID = 120), '"','>0</Setting>') as SetString
+ 	concat_ws('','\t\t<Setting Name="', invTypes.typeID, '"',
+	' ItemName="', REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(typeName,'&','&amp;'), '<','&lt;'), '>','&gt;'), '"','&quot;'), '\'','&apos;'), '"',
+	' GroupID="', groupID, '"',
+	' Metalevel="', IFNULL(IFNULL(metalevel.valueInt,metalevel.valueFloat),1), '"',
+	' Techlevel="', IFNULL(IFNULL(techLevel.valueInt,techLevel.valueFloat),1), '"',
+	' Volume="', volume, '"',
+	' Capacity="', capacity, '"',
+	' PortionSize="', portionSize, '"',
+	' BasePrice="', basePrice, '"',
+	' weaponRangeMultiplier="', IFNULL(IFNULL(weaponRangeMultiplier.valueFloat,weaponRangeMultiplier.valueInt),1), '"',
+	'>0</Setting>') as SetString
  FROM `invTypes`
- 	left outer join dgmTypeAttributes as TA on TA.typeID = invTypes.typeID and TA.attributeID=633
+ 	left outer join dgmTypeAttributes as metalevel on metalevel.typeID = invTypes.typeID and metalevel.attributeID=633
+ 	left outer join dgmTypeAttributes as weaponRangeMultiplier on weaponRangeMultiplier.typeID = invTypes.typeID and weaponRangeMultiplier.attributeID=120
+ 	left outer join dgmTypeAttributes as techLevel on techLevel.typeID = invTypes.typeID and techLevel.attributeID=422
  	order by invTypes.typeID
  INTO OUTFILE '/tmp/EVEDB_Items.xml'
  FIELDS ESCAPED BY ''
- LINES TERMINATED BY '\r\n'
+ LINES TERMINATED BY '\r\n';
+
 
 ; attributeID 633 is metalevel
 
@@ -23,4 +36,6 @@ SELECT
  order by staStations.solarSystemID
  into outfile '/tmp/EVEDB_Stations.xml'
 
- Dont forget to replace & with &amp; in new file before committing!
+SELECT 
+* FROM dgmTypeAttributes as dta
+left outer join dgmAttributeTypes as dat on dat.attributeID = dta.attributeID;
